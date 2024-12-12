d5f7250663d383363aa65270f79bbb9393d776d9
Mhirii <ahmedmhiri218@gmail.com>
2024-12-12 22:15:51 +0100

    remove useless comments

--- src/hashTable.h
+++ src/hashTable.h
@@ -8,24 +8,18 @@ struct HashTable {
 };
 
 
-/* Create a hash table. */
 struct HashTable *NewHashTable(char *name);
 
-/* Free a hash table. */
 int FreeHashTable(struct HashTable *table);
 
-/* Insert an item into the hash table. */
 int HashTableSet(struct HashTable *table, char *key, char *value);
 
-/* Find an item in the hash table. */
 char *HashTableGet(struct HashTable *table, char *key);
 
 int HashTableDelete(struct HashTable *table, char *key);
 
-/* Return the number of items in the hash table. */
 int HashTableCount(struct HashTable *table);
 
-/* Return the size of the hash table. */
 int HashTableSize(struct HashTable *table);
 
 unsigned int hash( char *key);
